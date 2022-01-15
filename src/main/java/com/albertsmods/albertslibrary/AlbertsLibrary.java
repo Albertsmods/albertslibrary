293b36d3d708f666af87bc9985f7aa45e95e0421
AlbertsMods <92393611+AlbertsMods@users.noreply.github.com>
2022-01-15 12:52:01 +0000

    initial commit

---
+++ src/main/java/com/albertsmods/albertslibrary/AlbertsLibrary.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) Albertsmods
+ * <p>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.albertsmods.albertslibrary;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(AlbertsLibrary.MOD_ID)
+public class AlbertsLibrary
+{
+    public static final String MOD_ID = "albertslibrary";
+
+    public AlbertsLibrary() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    private void setup(final FMLCommonSetupEvent event)
+    {
+    }
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+    }
+    private void processIMC(final InterModProcessEvent event)
+    {
+    }
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+    }
+}
