4cd575e2aeb15f0dbad11504be2a0ef6121d68ad
AlbertsMods <92393611+AlbertsMods@users.noreply.github.com>
2022-01-16 12:13:04 +0000

    v3.0.1 Refactor

---
+++ src/main/java/library/albertsmods/albertslibrary/block/custom/flammable/overworld/ModFlammableRotatedPillarBlock.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) Albertsmods
+ * <p>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package library.albertsmods.albertslibrary.block.custom.flammable.overworld;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class ModFlammableRotatedPillarBlock extends RotatedPillarBlock {
+
+    public ModFlammableRotatedPillarBlock(Properties p_55926_) {
+        super(p_55926_);
+    }
+
+    @Override
+    public boolean isFlammable(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+        return true;
+    }
+    @Override
+    public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+        return 5;
+    }
+    @Override
+    public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+        return 5;
+    }
+}
